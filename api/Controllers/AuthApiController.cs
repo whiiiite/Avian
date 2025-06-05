f2c929af1b80bd78f44205d10bdb9a4e186dad97
white <pavel1712171@gmail.com>
2025-06-05 21:27:30 +0300

    First Template of api

---
+++ api/Controllers/AuthApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvianApi.Controllers
+{
+    [ApiController]
+    public class AuthApiController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
